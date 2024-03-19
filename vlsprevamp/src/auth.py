3e5890e2efea1a4b8fa1235f70b54a2e7c1e0356
tadj <tadj.d.cazaubon@gmail.com>
2024-03-18 23:42:32 -0700

    _________

--- vlsprevamp/src/auth.py
+++ vlsprevamp/src/auth.py
@@ -2,13 +2,17 @@ from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__) 
 
+@auth.route("/signup")
+def signup():
+    return sign_up()
+
 @auth.route("/sign-up")
 def sign_up():
     return render_template("sign_up.html")
 
 @auth.route("/login")
 def login():
-    return "<p>login</p>"
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():

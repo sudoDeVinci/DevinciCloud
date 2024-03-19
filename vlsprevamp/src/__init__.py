c8d0428fd9a66895137dcadaf0d0bd5a8cadb280
Tadj Cazaubon <tadj.d.cazaubon@gmail.com>
2024-03-19 01:02:04 +0100

    changes to templates

--- vlsprevamp/src/__init__.py
+++ vlsprevamp/src/__init__.py
@@ -7,6 +7,6 @@ def create_app():
     from src.views import views
     from src.auth import auth
     app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/auth/")
+    app.register_blueprint(auth, url_prefix="/")
 
     return app
